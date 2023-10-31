c80ad327470e16eb04ea8cca5f67ec788d4e31dd
Olaus <saulobf8@gmail.com>
2023-10-30 21:29:28 -0300

    Alteração do App.py

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Definindo a variavel de ambiente
+os.environ['FLASK_DEBUG'] = True
+
+# Configurando o modo de depuração com base na variavel de ambiente
+app_debug = os.environ.get('FLASK_DEBUG') == 'True'
+
 @app.route('/')
 def ola():
     return render_template("index.html")
-app.run()
+
+
+if __name__ == "__main__":
+    app.run()
